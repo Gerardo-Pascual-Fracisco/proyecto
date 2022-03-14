4ac129cb9e110225c73cfff4a7c8706b3a011004
Darkneko0 <al221911729@gmail.com>
2022-03-14 10:25:32 -0600

    creando migracciones

---
+++ laravel/database/migrations/2022_03_14_155745_sale.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Sale extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale', function (Blueprint $table) {
+            $table->id();
+            $table->string('Total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale');
+    }
+}
