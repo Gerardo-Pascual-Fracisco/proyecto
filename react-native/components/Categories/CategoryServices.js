0372b8a7538593378d7969db619899bb5bc3dcce
Darkneko0 <al221911729@gmail.com>
2022-04-19 18:18:18 -0500

    obtener servicios de categoria

--- react-native/components/Categories/CategoryServices.js
+++ react-native/components/Categories/CategoryServices.js
@@ -1,72 +1,80 @@
-import React, {useEffect, useState} from "react";
-import { StyleSheet,Text, View ,FlatList,RefreshControl} from 'react-native'
-import {getCateServices} from '../../api'
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList, RefreshControl } from 'react-native'
+const MOSTRAR_SERVICES = 'http://192.168.1.72:8000/api/showById'
 
-export default function CategoryServices ({ navigation, route}) {
-  const {name,id} = route.params
+export default function CategoryServices({ navigation, route }) {
+
+  const { name, id } = route.params
+  const id_category=id;
   navigation.setOptions({ title: name })
 
-    console.log(route.params)
-    const [refreshing, setRefreshing] = React.useState(false);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [tasks, setTasks] = useState([])
 
-      const [tasks, setTasks] = useState([])
-  
-      const loadTasks = async () => {
-        const data = await getCateServices(route.params.id)
-        setTasks(data)
-        console.log(data);
-      };
-      useEffect(() => {
-        loadTasks();
-      }, []);
-
-      const renderItem = ({ item }) => {
-       
-      };
-    
-       const onRefresh = React.useCallback(async () => {
-
-        setRefreshing(true);
-        await loadTasks () ;
-        setRefreshing(false);
-    })
-      return (
-        <FlatList
-        style={{ width: '100%'}}
-          data={tasks}
-          keyExtractor={(item) => item.service_id + ""}
-          renderItem={renderItem}
-    
-          refreshControl={
-            <RefreshControl
-              refreshing={refreshing}
-              colors={["#78e08f"]}
-              onRefresh={onRefresh}
-              progressBackgroundColor="#000"
-        />
-    }
-    />
-    
-    );
+  //obtener servicios de categoria
+  const getCateServices = async (id=id_category) => {
+    const res = await fetch(`${MOSTRAR_SERVICES}/${id}`);
+    console.log(`${MOSTRAR_SERVICES}/${id}`);
+    return await res.json();
   };
-  const styles = StyleSheet.create({
-    itemContainer: {
-      backgroundColor: "#FFFFFF",
-      padding: 20,
-      marginVertical: 8,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      borderRadius: 5,
-    },
-    itemTitle: {
-      color: "#000000",
-    },
-    image: {
-      width: 50,
-      height: 50,
-    },
-  });
+
+  const loadTasks = async () => {
+    const data = await getCateServices()
+    setTasks(data.services)
+    console.log(data.services);
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const renderItem = ({ item }) => {
+<View><Text>{item.foto}</Text></View>
+  };
+
+  const onRefresh = React.useCallback(async () => {
+
+    setRefreshing(true);
+    await loadTasks();
+    setRefreshing(false);
+  })
+  return (
+    <FlatList
+      style={{ width: '100%' }}
+      data={tasks}
+      keyExtractor={(item) => item.id_service + ""}
+      renderItem={renderItem}
+
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          colors={["#78e08f"]}
+          onRefresh={onRefresh}
+          progressBackgroundColor="#000"
+        />
+      }
+    />
+
+  );
+};
+const styles = StyleSheet.create({
+  itemContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  itemTitle: {
+    color: "#000000",
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
 
 
 
